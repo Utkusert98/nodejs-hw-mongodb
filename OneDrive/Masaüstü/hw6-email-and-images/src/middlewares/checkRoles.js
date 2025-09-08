3b6bd6e45a0785e52ab41fe9ff06ab65b214411c
Utku Sert <utkusert.arc@gmail.com>
2025-09-08 14:39:15 +0300

    HW6: reset password + Cloudinary photo upload

---
+++ OneDrive/Masaüstü/hw6-email-and-images/src/middlewares/checkRoles.js
@@ -0,0 +1,26 @@
+import createHttpError from 'http-errors';
+
+import { ROLES } from '../constant/index.js';
+
+export const checkRoles = (...roles) => {
+  return (req, res, next) => {
+    const { user } = req;
+    if (!user) return next(createHttpError(401, 'Kimlik doğrulaması yap'));
+
+    const { role } = user;
+
+    if (!roles.includes(role)) {
+      return next(createHttpError(403, 'Yetkiniz yok'));
+    }
+
+    next();
+  };
+};
+
+//sadece admin kontrolü
+
+export const requireAdmin = checkRoles(ROLES.ADMIN);
+
+//Admin ve moderator kontrolü
+
+export const requireAdminOrModerator = checkRoles(ROLES.ADMIN, ROLES.MODERATOR);
---
+++ OneDrive/Masaüstü/hw6-email-and-images/src/utils/createDirIfNotExists.js
@@ -0,0 +1,11 @@
+import fs from 'fs/promises';
+
+export const createDirIfNotExists = async (url) => {
+  try {
+    await fs.access(url);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.mkdir(url, { recursive: true });
+    }
+  }
+};
